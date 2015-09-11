8d8d7066d4414f0b528e877a25a5af333cacbd2f
Luvsandondov Lkhamsuren <lkhamsurenl@gmail.com>
2015-09-10 22:24:46 -0700

    DP fixed solution for minimal number of squares adding up to a number

--- numOfPerfectSquaresSum.py
+++ numOfPerfectSquaresSum.py
@@ -15,12 +15,16 @@ class Solution(object):
 			ls[i] = 1
 		# recursive 
 		for i in range(1, num + 1):
-			if i not in ls:
+			if i not in squares:
 				minimal_sq = float('inf')
 				# find the index of the number closest but less than i
 				max_index = self.binarySearch(i, squares, 0, len(squares) - 1)
-				for num_sq in squares[:max_index]:
-					minimal_sq = min(minimal_sq, 1 + ls[i - num_sq])
+				for num_sq in squares[:max_index+1]:
+					# num_sq can be repeated in the list
+					times = 1
+					while times * num_sq <= i:
+						minimal_sq = min(minimal_sq, 1 + ls[i - num_sq])
+						times += 1
 				ls[i] = minimal_sq
 		return ls[num]
 
@@ -31,14 +35,29 @@ class Solution(object):
 		if low >= high:
 			return low
 		mid = low + (high - low) / 2
+		# middle is the correct answer
 		if arr[mid] > num and arr[mid - 1] <= num:
 			return mid
 		if arr[mid] < num:
-			return self.binarySearch(num, arr, mid, high)
+			return self.binarySearch(num, arr, mid + 1, high)
 		else:
-			return self.binarySearch(num, arr, low, mid)
+			return self.binarySearch(num, arr, low, mid - 1)
 
 sol = Solution()
 assert(1 == sol.numSquares(1))
 assert(1 == sol.numSquares(4))
+# 2 = 1 + 1
 assert(2 == sol.numSquares(2))
+# 9 = 9
+assert(1 == sol.numSquares(9))
+# 12 = 4 + 4 = 4
+assert(3 == sol.numSquares(12))
+# 13 = 9 + 4
+assert(2 == sol.numSquares(13))
+print(sol.numSquares(296))
+assert(2 == sol.numSquares(17))
+########## Testing binarySearch
+sq = [1, 4, 9,16,25,36,49,64,81,100]
+assert(1 == sol.binarySearch(3, sq, 0, 9))
+assert(2 == sol.binarySearch(5, sq, 0, 9))
+assert(1 == sol.binarySearch(5, sq, 0, 1))
