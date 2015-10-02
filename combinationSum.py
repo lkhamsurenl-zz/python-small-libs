b99109a552e5ddf8a0509822ca5a015c19f2646a
Luvsandondov Lkhamsuren <lkhamsurenl@gmail.com>
2015-10-01 19:59:36 -0700

    combination sum try

--- combinationSum.py
+++ combinationSum.py
@@ -1,30 +1,32 @@
 import copy
+import itertools
 class Solution(object):
-	sol = []
-	def combinationSum3(self, k, n):
-		self.recCom(0, k, n, [])
-		return self.sol
+	out = []
+	def combinationSum(self, candidates, target):
+		self.recCom(candidates, 0, target, [0 for i in range(len(candidates))])
+		return self.out
 
-	def recCom(self, i, k, n, currList):
-		if i > 9: 
-			if k != 0 or n != 0:
-				return
-			else:
-				self.sol.append(currList)
-				return 
-		else:
-			# current index is included
+	def recCom(self, candidates, index, target, currList):
+		if target < 0 or index < len(candidates):
+			return
+		if target == 0:
+			lst = []
+			for i in range(len(currList)):
+				time = currList[i] # candidates[i] should appear time in list
+				lst = lst + [candidates[i] for j in range(time)]
+			self.out.append(lst)
+			return
+		times = 0
+		while times * candidates[index] <= target:
 			newList = copy.deepcopy(currList)
-			newList.append(i)
-			self.recCom(i + 1, k - 1, n - i, newList)
-			# not included
-			self.recCom(i + 1, k, n, currList)
+			newList[index] = times
+			self.recCom(candidates, index + 1, target - times * candidates[index], newList)
+			times += 1
 
 ########################## Test ###################################
 sol = Solution()
 
 ########################## combination sum ###################################
 print("combination sum test")
-assert([[1,2,4]] == sol.combinationSum3(3,7))
-assert([[1,2,6], [1,3,5], [2,3,4]] == sol.combinationSum3(3,9))
+print(sol.combinationSum([5, 10, 8, 4, 3, 12, 9], 27))
 print("all test are passed!")
