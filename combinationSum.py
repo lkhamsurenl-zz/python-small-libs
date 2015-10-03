ae3fb27b840822f0d2dcbed4fb03ea15be9a5c06
Luvsandondov Lkhamsuren <lkhamsurenl@gmail.com>
2015-10-03 13:41:22 -0700

    combination sum of digits adding up to n with using exactly k digits.

--- combinationSum.py
+++ combinationSum.py
@@ -35,6 +35,35 @@ class Solution(object):
 			newNums.remove(num)
 			self.dfs(newNums, target - num, currList + [num], globalList)
 
+	def combinationSum3(self, n, k):
+		"""
+		Given n, find k digits between 1 to 9 add up to n such that no digit is repeated.
+		1 <= k <= 9
+		1 <= n <= 45
+		"""
+		result = []
+		if not (1 <= k <= 9 and 1<= n <= 45):
+			return result
+		self.digitSum(n, k, [], 0, result)
+		return result
+
+	def digitSum(self, n, k, currList, index, result):
+		"""
+		recursive function to compute all ways to get the n
+		@param n: number to add up to
+		@param k: number of digits allowed to use
+		@param currList: current list of numbers used
+		@param index: can use digits between index < i <= 9
+		@param result: overall result of all possible answer
+		"""
+		if index > 9 or k < 0 or (n != 0 and k == 0) or (n == 0 and k != 0):
+			return # backtrack
+		if n == 0 and k == 0:
+			result.append(currList)
+			return
+		for i in range(index + 1, 10):
+			self.digitSum(n - i, k - 1, currList + [i], i, result)
+
 
 ########################## Test ###################################
 sol = Solution()
@@ -49,4 +78,10 @@ print("all test are passed!")
 print("combination sum2 test")
 print(sol.combinationSum2([10,1,2,7,6,1,5],8))
 print(sol.combinationSum2([1,2,3,4], 6))
+print("all test are passed!")
+
+########################## combination sum3 ###################################
+print("combination sum3 test")
+assert([[1,2,4]] == sol.combinationSum3(7, 3), "wanted: {}\ngot: ".format([[1,2,4]], sol.combinationSum3(7,3)))
+assert([[1,2,6], [1,3,5], [2,3,4]] == sol.combinationSum3(9, 3), "wanted: {}\ngot: ".format([[1,2,6], [1,3,5], [2,3,4]], sol.combinationSum3(9,3)))
 print("all test are passed!")
