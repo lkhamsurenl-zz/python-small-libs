652ddd09004f000a98d4228d30a55ecb696b4523
Luvsandondov Lkhamsuren <lkhamsurenl@gmail.com>
2015-11-08 15:01:21 -0800

    clean up liked list

--- linkedList.py
+++ linkedList.py
@@ -4,7 +4,27 @@ class ListNode(object):
         self.val = x
         self.next = None
 
-class Solution(object):
+    def __str__(self):
+        if self.next == None:
+            return str(self.val)
+        return "{}->{}".format(self.val, self.next)
+
+class LL(object):
+    def fromList(self, lst):
+        head = ListNode(-1) # dummy head
+        curr = head # iterator
+        i = 0 # iterator on lst
+        while i < len(lst):
+            curr.next = ListNode(lst[i])
+            i += 1
+            curr = curr.next
+        return head.next
+
+    def equal(self, head1, head2):
+        if head1 == None or head2 == None:
+            return head1 == head2 # since at least one is None, they should be same
+        return head1.val == head2.val and self.equal(head1.next, head2.next)
+
     # check if LL is a palindrome
     def isPalindrome(self, head):
         # find the middle element
@@ -23,6 +43,8 @@ class Solution(object):
 
     # find middle of linked list
     def findMiddle(self, head):
+        if head == None or head.next == None:
+            return head
         slow = head
         fast = head
         while fast.next != None and fast.next.next != None:
@@ -55,6 +77,49 @@ class Solution(object):
             last = last.next
         return nth
 
+    def removeNthLast(self, head, n):
+        """
+        removeNthLast element in the given LL, 1 <= n <= len
+        """
+        # Find the n + 1 the last element, then remove next one
+        dummy = ListNode(-1) # dummy head
+        dummy.next = head
+        prev = self.findNthLast(dummy, n + 1)
+        # fix the next pointer
+        prev.next = prev.next.next
+        return dummy.next
+
+    def mergeSorted(self, head1, head2):
+        if head1 == None:
+            return head2
+        if head2 == None:
+            return head1
+        # ensure head1 value is always smaller than that of head2
+        if head1.val > head2.val:
+            temp = head1
+            head1 = head2
+            head2 = temp
+        head = head1 # this will be the head after merging
+        while head1.next != None and head2 != None:
+            # compare values in head1.next and head2
+            if head2.val < head1.next.val:
+                temp = head1.next
+                head1.next = head2
+                head2 = temp
+            head1 = head1.next
+        # If head2 is not done, just join since head1.next is None.
+        if head2 != None:
+            head1.next = head2
+        return head
+
+    def mergeSort(self, head):
+        if head == None or head.next == None:
+            return head
+        mid = self.findMiddle(head)
+        head2 = mid.next
+        mid.next = None
+        return self.mergeSorted(self.mergeSort(head), self.mergeSort(head2))
+
     def linkedListCopy(self, head):
         """
         Given a linked list with pointers, copy
@@ -77,95 +142,75 @@ class Solution(object):
         return dummy.next
 
 ################################ TEST   #############################
-sol = Solution()
+ll = LL()
 ############################### reverse test ##########################
-print("reverse LL test")
-head = ListNode(1)
-head.next = ListNode(2)
-head.next.next = ListNode(3)
-rev = sol.reverse(head)
-while rev != None:
-    print(rev.val)
-    rev = rev.next
+for (l, w) in [ ([1,2,3], [3,2,1]), ([1], [1]) ]:
+    head = ll.fromList(l)
+    want = ll.fromList(w)
+    got = ll.reverse(head)
+    assert ll.equal(got, want), \
+        "reverse({}) = {}; want {}".format(head, got, want)
 
-############################### reverse test ##########################
-print("Middle LL test")
-head = ListNode(1)
-head.next = ListNode(2)
-head.next.next = ListNode(3)
-mid = sol.findMiddle(head)
-assert(mid.val == 2)
-
-head = ListNode(1)
-head.next = ListNode(2)
-head.next.next = ListNode(3)
-head.next.next.next = ListNode(1)
-mid = sol.findMiddle(head)
-assert(mid.val == 2)
-print("Middle LL tests passed!")
+############################### find Middle ##########################
+for (l, w) in [ ([1,2,3], 2), ([1,2,3,1], 2) ]:
+    head = ll.fromList(l)
+    got = ll.findMiddle(head)
+    want = ListNode(w)
+    assert (got == None and want == None) or got.val == want.val, \
+        "findMiddle({}) = {}; want {}".format(head, got.val, want)
 
 ############################### Nth last test ##########################
-print("Nth Last LL test")
-head = ListNode(1)
-head.next = ListNode(2)
-head.next.next = ListNode(3)
-mid = sol.findNthLast(head, 1)
-assert(mid.val == 3)
-mid = sol.findNthLast(head, 2)
-assert(mid.val == 2)
-mid = sol.findNthLast(head, 3)
-assert(mid.val == 1)
+for (l, i, w) in [ ([1,2,3], 1, 3), ([1,2,3], 2, 2), ([1,2,3], 3, 1), ([1], 1, 1) ]:
+    head = ll.fromList(l)
+    got = ll.findNthLast(head, i)
+    want = ListNode(w)
+    assert (got == None and want == None) or got.val == want.val, \
+        "findNthLast({}, {}) = {}; want {}".format(head, i, got, want)
 
-head = ListNode(1)
-mid = sol.findNthLast(head, 1)
-assert(mid.val == 1)
-
-print("None case")
-head = None
-mid = sol.findNthLast(head, 1)
-assert(mid.val == -1)
-print("FindNthLast tests passed!")
+############################### remove Nth last test ##########################
+for (l, i, w) in [ ([1,2,3], 1, [1,2]), ([1,2,3], 2, [1,3]), ([1,2,3], 3, [2,3]), ([1], 1, []) ]:
+    head = ll.fromList(l)
+    got = ll.removeNthLast(head, i)
+    want = ll.fromList(w)
+    assert ll.equal(got, want), \
+        "removeNthLast({}, {}) = {}; want {}".format(head, i, got, want)
 
 ############################### is Palindrome ##########################
-print("LL palindrome test")
-# only one element
-head = ListNode(1)
-assert(True == sol.isPalindrome(head))
+for (l, want) in [ ([1,2,3], False), ([1,2,2,1], True), ([1,2,3,2,1], True) ]:
+    head = ll.fromList(l)
+    got = ll.isPalindrome(head)
+    assert got == want, \
+        "isPalindrome({}) = {}; want {}".format(head, got, want)
 
-# simple non palindrome odd case
-head = ListNode(1)
-head.next = ListNode(2)
-head.next.next = ListNode(3)
-assert(False == sol.isPalindrome(head))
+############################### Merge sorted LL ##########################
+for (l1, l2, w) in [ ([1], [2], [1,2]), ([], [], []), ([1,4,5], [2,3,6], [1,2,3,4,5,6]), ([2,3,6], [1,4,5], [1,2,3,4,5,6]) ]:
+    head1 = ll.fromList(l1)
+    head2 = ll.fromList(l2)
+    got = ll.mergeSorted(head1, head2)
+    want = ll.fromList(w)
+    assert ll.equal(got, want), \
+        "mergeSorted({}, {}) = {}; want {}".format(head1, head2, got, want)
 
-# even palindrome case
-head = ListNode(1)
-head.next = ListNode(2)
-head.next.next = ListNode(2)
-head.next.next.next = ListNode(1)
-assert(True == sol.isPalindrome(head))
-
-# odd number of elements
-head = ListNode(1)
-head.next = ListNode(2)
-head.next.next = ListNode(3)
-head.next.next.next = ListNode(2)
-head.next.next.next.next = ListNode(1)
-assert(True == sol.isPalindrome(head))
-print("isPalindrome LL tests passed!")
+############################### Merge sort on LL ##########################
+for (l, w) in [ ([], []), ([1,4,5, 2,3,6], [1,2,3,4,5,6]), ([2,3,6,1,4,5], [1,2,3,4,5,6]) ]:
+    head = ll.fromList(l)
+    got = ll.mergeSort(head)
+    want = ll.fromList(w)
+    assert ll.equal(got, want), \
+        "mergeSort({}) = {}; want {}".format(head, got, want)
 
 ########################### Linked list copy pointer ######################
-print("linked list wiht pointer copy")
+print("linked list with pointer copy")
 head = ListNode(None)
 head.val = ListNode(None)
 head.next = ListNode(None)
 head.next.val = head
 head.next.next = None
 
-copy = sol.linkedListCopy(head)
+copy = ll.linkedListCopy(head)
 assert(copy == copy.next.val)
 assert(copy.next.next == None)
 
 head = None
-copy = sol.linkedListCopy(head)
+copy = ll.linkedListCopy(head)
 assert(copy == None)
