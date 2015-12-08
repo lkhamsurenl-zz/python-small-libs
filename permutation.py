6fccd0d969a7f2e172457c3fd3ac531bfc4b8fe2
Luvsandondov Lkhamsuren <lkhamsurenl@gmail.com>
2015-12-08 15:39:19 -0800

    Clean up the permutation

--- permutation.py
+++ permutation.py
@@ -1,4 +1,4 @@
-import copy
+import copy, math
 class Solution:
     # @param num, a list of integer
     # @return a list of lists of integers
@@ -24,6 +24,23 @@ class Solution:
             currList.append(currInList)
         return currList
 
+    def getPermutation(self, n, k):
+        return self.recPerm([i for i in range(1, n + 1)], k)
+
+    def recPerm(self, lst, k):
+        if k == 0:
+            return "".join(map(str, lst))
+        n = len(lst)
+        fac = math.factorial(n - 1)
+        firstDigInd = k / fac  # first digit index in list
+        firstDig = lst[firstDigInd]
+        lst.remove(lst[firstDigInd]) # remove the digit
+        return str(firstDig) + self.recPerm(lst, k - fac * firstDigInd)
+
 sol = Solution()
+for n in [3, 4, 5]:
+    for k in range(math.factorial(n)):
+        print("{} perm in 1 to {} is {}".format(k, n, sol.getPermutation(n, k)))
+
 for num in [[1, 2, 3] , [1, 2, 3,4], [1,2]]:
     print("permutation of {} is {}".format(num, sol.permute(num)))
